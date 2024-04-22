b8931621147f71a60a902e1bc982ab84c0887b03
AxelAl <axel.alzate.99@gmail.com>
2024-04-22 10:16:03 -0500

    implementacion codig para manejar los botones

--- Assets/UI/FireBaseController.cs
+++ Assets/UI/FireBaseController.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FireBaseController : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    public GameObject login,signup,profile;
+    public InputField Logemail,logpass,Sigpass,Sigemail,SigCpass,Siguser;
+    
+    public void Oplog(){
+        login.SetActive(true);
+        signup.SetActive(false);
+        profile.SetActive(false);
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void Opsignup(){
+        login.SetActive(false);
+        signup.SetActive(true);
+        profile.SetActive(false);
+    }
+    public void Opprofile(){
+        login.SetActive(false);
+        signup.SetActive(false);
+        profile.SetActive(true);
+    }
+    public void LoginUser(){
+        if(string.IsNullOrEmpty(Logemail.text)&&string.IsNullOrEmpty(logpass.text)){
+            return;
+        }
+        //log in 
+    }
+    public void SignUpUser(){
+        if(string.IsNullOrEmpty(Sigemail.text)&&string.IsNullOrEmpty(Sigpass.text)&&string.IsNullOrEmpty(SigCpass.text)&&string.IsNullOrEmpty(Siguser.text)){
+            return;
+        }
+        //Registro 
     }
 }
