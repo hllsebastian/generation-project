89ec8fc6b7d369572464f2fcb1129af761e67fd6
AxelAl <axel.alzate.99@gmail.com>
2024-05-04 04:18:13 -0500

    poolenemy

--- Assets/Scenes/Scripts/AdministradorDeEscenas.cs
+++ Assets/Scenes/Scripts/AdministradorDeEscenas.cs
@@ -7,20 +7,18 @@ public class AdministradorDeEscenas : MonoBehaviour
 {
     /*public GameObject Canvas; // Referencia al canvas para retener imagenes
     public GameObject eventos;*/
+    [SerializeField] private GameObject Rune;
     void Awake()
     {
         //DontDestroyOnLoad(this.gameObject); // Mantén este objeto
     }
+private void Update() {
+    if(Rune.activeSelf){
+        Debug.Log("hola");
+        gameObject.SetActive(true);
+    }
+}
 
-    /* public void CambiarEscena(int nombreDeLaEscena)
-     {
-         // Guarda los objetos que quieres mantener
-         DontDestroyOnLoad(Canvas);
-         DontDestroyOnLoad(eventos);
-
-         // Carga la nueva escena
-         SceneManager.LoadScene(nombreDeLaEscena);
-     }*/
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("Collision Detected with " + other.gameObject.name);
